5ab785dcd1f19ecb85cdd9e07d187ee2d097cf4e
antoine.lizee <antoine.lizee@gmail.com>
2015-01-26 14:48:20 -0800

    put the testing files in the test folder

--- GSC.R
+++ GSC.R
@@ -1,16 +1,14 @@
-# Implementation of the Gerstein-Sonnhammer-Chothia algorithm
+# Implementation of the Gerstein-Sonnhammer-Chothia algorithm.
 #
 # This script makes discoverable the funtion "GSC" that takes as argument a dendrogram and returns
 # a named vector of weight for each of the leaves of the input dendrogram. These weigths are supposed
 # to account for similarity between objects when considering them in a model. The more similar, the 
 # lower the weights.
-# The initial description of the implementation can be found here: https://pdf.yt/d/Sx3jMbr8vANgxAej/download
-# As per request for: http://thinklab.org/p/rephetio/seeking-an-open-source-implementation-of-the-gerstein-sonnhammer-chothia-algorithm/25
+# The algorithm was initially descriped in:
+# > Gerstein M, Sonnhammer ELL, Chothian C (1994) Volume Changes in Protein Evolution.
+# > Journal of Molecular Biology. doi:10.1016/0022-2836(94)90012-4
 #
 # Copyright Antoine Lizee 2015/01 antoine.lizee@gmail.com
-
-
-# Algorithm ---------------------------------------------------------------------
 
 # getGSCs is a handler for the main recursve funtion below. It actually does all the work:
 # It applies the main function to the child tree and does the main weighing computation,
@@ -65,29 +63,4 @@ GSC <- function(dd) {
   }, 
   finally = {options("expressions" = opt.old)}
   )
-}
-
-# Tests -------------------------------------------------------------------
-
-if (test <- TRUE) { # Change to TRUE to launch testing code
-  
-  # This is the actual example of the paper. The reader should be warned that the results in the paper are
-  # wrong, due to some imprecision.
-  testhc <- list("merge" = matrix(nrow = 3, byrow = T, c(-1,-2,-3,1, -4,2)),
-                 "height" = c(20, 50, 80),
-                 "order" = c(1,2,3,4),
-                 "labels" = LETTERS[1:4])
-  attr(testhc, "class") <- "hclust"
-  testdend <- as.dendrogram(testhc)
-  plot(testdend)
-  print(GSC(testdend))
-  
-  # A more real-looking use-case:
-  hc <- hclust(dist(mtcars))
-  dd <- as.dendrogram(hc)
-  plot(as.dendrogram(hc))
-  ddGSCs <- GSC(dd) #compute the weights
-  hc$labels[hc$order] <- paste(names(ddGSCs), sprintf("%.1f", ddGSCs), sep = " - ") #add them to the label names
-  plot(as.dendrogram(hc)) #plot them to have a look.
-  
 }
