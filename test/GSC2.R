0fc9730de75dda98c4cbaf56ccfe35432f2937e2
antoine.lizee <antoine.lizee@gmail.com>
2015-01-26 16:09:01 -0800

    removed the test part of GSC2.R

--- test/GSC2.R
+++ test/GSC2.R
@@ -38,27 +38,3 @@ GSC2 <- function(dd) {
   return(GSCs / sum(GSCs) * length(GSCs))
 }
 
-
-# test --------------------------------------------------------------------
-
-if (test <- FALSE) { # Change to TRUE to launch testing code
-  
-  # This is the actual example of the paper. The reader should be warned that the results in the paper are
-  # wrong, due to some imprecision.
-  testhc <- list("merge" = matrix(nrow = 3, byrow = T, c(-1,-2,-3,1, -4,2)),
-                 "height" = c(20, 50, 80),
-                 "order" = c(1,2,3,4),
-                 "labels" = LETTERS[1:4])
-  attr(testhc, "class") <- "hclust"
-  testdend <- as.dendrogram(testhc)
-  plot(testdend)
-  print(GSC2(testdend))
-  
-  # A more real-looking use-case:
-  hc <- hclust(dist(mtcars))
-  dd <- as.dendrogram(hc)
-  plot(as.dendrogram(hc))
-  ddGSCs <- GSC2(dd) #compute the weights
-  hc$labels[hc$order] <- paste(names(ddGSCs), sprintf("%.1f", ddGSCs), sep = " - ") #add them to the label names
-  plot(as.dendrogram(hc)) #plot them to have a look.
-}
